7c08059f64608b2636239d13c3025267719c176b
pm <przemekmarkowski@o2.pl>
2019-07-11 16:01:35 +0200

    add testing coroutines implementations

--- test/tasks/Request6ProgressKtTest.kt
+++ test/tasks/Request6ProgressKtTest.kt
@@ -3,24 +3,28 @@ package tasks
 import contributors.MockGithubService
 import contributors.progressResults
 import contributors.testRequestData
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
 import org.junit.Test
 
+@UseExperimental(ExperimentalCoroutinesApi::class)
 class Request6ProgressKtTest {
     @Test
-    fun testProgress() = runBlocking {
-        val startTime = System.currentTimeMillis()
+    fun testProgress() = runBlockingTest {
+        val startTime = currentTime //System.currentTimeMillis()
         var index = 0
-        loadContributorsProgress(MockGithubService, testRequestData) {
-            users, _ ->
+        loadContributorsProgress(MockGithubService, testRequestData) { users, _ ->
             val expected = progressResults[index++]
-            val time = System.currentTimeMillis() - startTime
-            /*
+            val time = currentTime - startTime //System.currentTimeMillis() - startTime
+
             // TODO: uncomment this assertion
-            Assert.assertEquals("Expected intermediate result after virtual ${expected.timeFromStart} ms:",
-                expected.timeFromStart, time)
-            */
+            Assert.assertEquals(
+                "Expected intermediate result after virtual ${expected.timeFromStart} ms:",
+                expected.timeFromStart, time
+            )
+
             Assert.assertEquals("Wrong intermediate result after $time:", expected.users, users)
         }
     }
