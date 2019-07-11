5e9a54b01aec0bb5c6fcf4d1e1331d6ee5d1fc2c
pm <przemekmarkowski@o2.pl>
2019-07-11 15:10:38 +0200

    add request6progress implementation

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,34 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+//    val users = mutableListOf<User>()
+//
+//    repos.forEach { repo ->
+//        val contributors = service
+//            .getRepoContributors(req.org, repo.name)
+//            .also { logUsers(repo, it) }
+//            .bodyList()
+//
+//        users += contributors
+//        updateResults(users.aggregate(), false)
+//
+//    }
+//
+//    updateResults(users.aggregate(), true)
+
+    var allUsers = emptyList<User>()
+    for ((index, repo) in repos.withIndex()) {
+        val users = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+
+        allUsers = (allUsers + users).aggregate()
+        updateResults(allUsers, index == repos.lastIndex)
+    }
 }
